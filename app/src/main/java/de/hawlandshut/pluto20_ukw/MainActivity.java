f016b32b320a3d9a6f7fa27fdfb30f3474f61aaa
dgreipl <dgreipl@hawlandshutde.onmicrosoft.com>
2019-10-10 11:05:26 +0200

    Code after T1 (all activities are created)

--- app/src/main/java/de/hawlandshut/pluto20_ukw/MainActivity.java
+++ app/src/main/java/de/hawlandshut/pluto20_ukw/MainActivity.java
@@ -3,6 +3,8 @@ package de.hawlandshut.pluto20_ukw;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +15,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate called");
+        Toast.makeText( getApplicationContext(), "Hat funktioniert", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy called");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart called");
     }
 }
