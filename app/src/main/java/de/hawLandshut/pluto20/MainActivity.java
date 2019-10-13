2f17e4b9d37eaa0661d3c6d074b17448c0786ab3
marti <mpad197@gmx.de>
2019-10-13 12:25:01 +0200

    options menut test implemented

---
+++ app/src/main/java/de/hawLandshut/pluto20/MainActivity.java
@@ -0,0 +1,94 @@
+package de.hawLandshut.pluto20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "XX MyActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "my log onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "my log onResume");
+        Toast.makeText(getApplicationContext(), "Your Pluto20 was been resumed!", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "my log onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "my log onPause");
+        Toast.makeText(getApplicationContext(), "Your Pluto20 was been stopped", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "my log onDestroy");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater;
+        inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.mainMenuPost:
+                Log.d(TAG, "Post was pressed");
+                return true;
+
+            case R.id.mainMenuHelp:
+                Log.d(TAG, "Help was pressed");
+                return true;
+
+            case R.id.mainMenuTest:
+                Log.d(TAG, "Test was pressed");
+                return true;
+
+            case R.id.mainMenuManageAccount:
+                Log.d(TAG, "ManageAccount was pressed.");
+                return true;
+
+            case R.id.idWriteTime:
+                Toast.makeText(getApplicationContext(), "You want to write the time :D", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
