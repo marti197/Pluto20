9a6940f3e70a4ba9e8dd643b23824aa7504bd6ee
dgreipl <dgreipl@hawlandshutde.onmicrosoft.com>
2019-11-05 14:43:16 +0100

    Changed applicationId in gradle file of app

--- app/src/main/java/de/hawlandshut/pluto20_ukw/ManageAccountActivity.java
+++ app/src/main/java/de/hawlandshut/pluto20_ukw/ManageAccountActivity.java
@@ -3,12 +3,15 @@ package de.hawlandshut.pluto20_ukw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ManageAccountActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TextView textview = (TextView) findViewById( R.id.manageAccountTechnicalId);
         setContentView(R.layout.activity_manage_account);
     }
 }
